3e5aa1aa9c820b638ed0031cd3c2d87b4aeba26b
Lou Berger <lberger@labn.net>
2017-09-02 11:55:50 -0400

    rfp-ovs-of: reformat using indent.py
    Signed-off-by: Lou Berger <lberger@labn.net>

--- bgpd/rfp-ovs-of/librfp/rfp_internal.h
+++ bgpd/rfp-ovs-of/librfp/rfp_internal.h
@@ -1,4 +1,4 @@
-/* 
+/*
  *
  * Copyright 2015-2017, LabN Consulting, L.L.C.
  *
@@ -25,27 +25,25 @@
 
 #include "rfp_ovs_of.h"
 
-extern int rfp_ovs_of_main (int argc, char *argv[]);
-extern int rfp_ovs_of_listen (void *vooi, uint16_t port);
-extern int rfp_ovs_of_close_all (void *vooi);
-extern int rfp_ovs_of_switch_close (void *vooi, void *rfd, int reason);
-extern const char *rfp_ovs_of_set_loglevel (const char *ovs_lvl);
-extern void *rfp_ovs_of_init (void *parent, int argc, char *argv[],
-                              const char *ovs_lvl);
-extern void rfp_ovs_of_destroy (void *vooi);
-extern void rfp_ovs_of_reset_all (void *vooi);
-extern int
-rfp_ovs_of_reset_all_by_ss (void *vooi, struct sockaddr_storage *ss);
-extern int
-rfp_ovs_of_reset_all_by_dpid (void *vooi, unsigned long long int dpid);
-extern void rfp_ovs_of_reset_flows (void *vooi);
-extern int
-rfp_ovs_of_reset_flow_by_ss (void *vooi, struct sockaddr_storage *ss);
-extern int
-rfp_ovs_of_reset_flow_by_dpid (void *vooi, unsigned long long int dpid);
-typedef void (rfp_fd_cb_t) (void *vooi, int fd, void *data);
-extern void
-rfp_fd_listen (void *parent, int fd, rfp_fd_cb_t * cb, void *data);
-extern int rfp_fd_close (void *parent, int fd);
-extern void rfp_ovs_of_show_switches(void *vty, void *vooi, unsigned long long int dpid);
+extern int rfp_ovs_of_main(int argc, char *argv[]);
+extern int rfp_ovs_of_listen(void *vooi, uint16_t port);
+extern int rfp_ovs_of_close_all(void *vooi);
+extern int rfp_ovs_of_switch_close(void *vooi, void *rfd, int reason);
+extern const char *rfp_ovs_of_set_loglevel(const char *ovs_lvl);
+extern void *rfp_ovs_of_init(void *parent, int argc, char *argv[],
+			     const char *ovs_lvl);
+extern void rfp_ovs_of_destroy(void *vooi);
+extern void rfp_ovs_of_reset_all(void *vooi);
+extern int rfp_ovs_of_reset_all_by_ss(void *vooi, struct sockaddr_storage *ss);
+extern int rfp_ovs_of_reset_all_by_dpid(void *vooi,
+					unsigned long long int dpid);
+extern void rfp_ovs_of_reset_flows(void *vooi);
+extern int rfp_ovs_of_reset_flow_by_ss(void *vooi, struct sockaddr_storage *ss);
+extern int rfp_ovs_of_reset_flow_by_dpid(void *vooi,
+					 unsigned long long int dpid);
+typedef void(rfp_fd_cb_t)(void *vooi, int fd, void *data);
+extern void rfp_fd_listen(void *parent, int fd, rfp_fd_cb_t *cb, void *data);
+extern int rfp_fd_close(void *parent, int fd);
+extern void rfp_ovs_of_show_switches(void *vty, void *vooi,
+				     unsigned long long int dpid);
 #endif /* _RFP_INTERNAL_H */
